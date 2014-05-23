ad43390bee61d84f59e055febe2b3d3f82e4b7d8
rhouli <ryan.houlihan90@gmail.com>
2014-05-22 23:05:13 -0700

    Initial Commit

---
+++ tranGOut/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tranGOut
+//
+//  Created by Ryan on 5/22/14.
+//  Copyright (c) 2014 Ryan Houlihan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tranGOutTests/tranGOutTests.m
@@ -0,0 +1,34 @@
+//
+//  tranGOutTests.m
+//  tranGOutTests
+//
+//  Created by Ryan on 5/22/14.
+//  Copyright (c) 2014 Ryan Houlihan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface tranGOutTests : XCTestCase
+
+@end
+
+@implementation tranGOutTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
